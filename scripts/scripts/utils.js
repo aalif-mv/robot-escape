ea26baac430d13ad42592f178bd25b7a12793687
softdev00 <89693227+softdev00@users.noreply.github.com>
2022-03-29 23:35:59 +0500

    Delete scripts/scripts directory

--- scripts/scripts/utils.js
+++
@@ -1,9 +0,0 @@
-function hypotenuse(op, aj) {
-    return Math.sqrt(Math.pow((op), 2) + Math.pow((aj), 2));
-}
-function crossMultiply(h, ph, oa) {
-    return (oa * h) / ph;
-}
-function random(min, max) {
-    return Math.floor((Math.random() * (max - min))+ min);
-}
